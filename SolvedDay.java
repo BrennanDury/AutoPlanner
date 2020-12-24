94c36236dd11f5f94fb21574930f77d53077aeb3
BrennanDury <brennandury@gmail.com>
2020-12-23 23:07:29 -0800

    Update SolvedDay.java
    Implements adding minutes of a task

--- SolvedDay.java
+++ SolvedDay.java
@@ -5,8 +5,16 @@ public class SolvedDay {
     private TreeMap<LooseTask, Integer> minutesEachTask;
     private TreeSet<StrictTask> strictTasks;
 
+    public SolvedDay() {
+        this.minutesEachTask = new TreeMap<LooseTask, Integer>();
+        this.strictTasks = new TreeSet<StrictTask>();
+    }
     public void addMinutesOfTask(LooseTask task, int minutes) {
-
+        if (!minutesEachTask.containsKey(task)) {
+            minutesEachTask.put(task, minutes);
+        } else {
+            minutesEachTask.put(task, minutesEachTask.get(task) + minutes);
+        }
     }
 
     public void addStrictTask(StrictTask task) {
